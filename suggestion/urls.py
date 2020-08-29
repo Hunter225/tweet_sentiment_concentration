25d59137440fb678cefdfd602d454fe792e44762
Hunter Sze <chsze@connect.ust.hk>
2020-08-29 17:28:50 +0800

    debug for url

--- suggestion/urls.py
+++ suggestion/urls.py
@@ -2,4 +2,4 @@ from django.conf.urls import url
 from rest_framework import routers
 from .views import SuggestionViewSet
 
-urlpatterns = [url(r'^api/sentiment/suggestion/(?P<suggestion_date>[^\s]+)$', SuggestionViewSet.as_view(), name='suggestion')]
+urlpatterns = [url(r'^api/sentiment/suggestion/(?P<suggestion_date>[^\s]+)/$', SuggestionViewSet.as_view(), name='suggestion')]
