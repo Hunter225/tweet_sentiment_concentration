f68e98eecaab21a038eeb7c9bc94fa05a9d22085
Hunter Sze <chsze@connect.ust.hk>
2020-09-01 21:49:23 +0800

    debug for connection issue

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 bash -c "/usr/local/bin/python3.6 manage.py crontab add"
-bash -c "/usr/local/bin/python3.6 manage.py runserver 127.0.0.1:8081"
+bash -c "/usr/local/bin/python3.6 manage.py runserver 0.0.0.0:8081"
